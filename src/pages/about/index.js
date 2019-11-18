daf5304f676860bdb28980e03a9f0bea8f4f7cb0
靳炳淑 <jinbingshu_nju@qq.com>
2019-11-18 23:24:55 +0800

    about

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -5,6 +5,7 @@ import RyMap from "../../components/RyMap";
 import Location from "./components/Location";
 import Teachers from "./components/Teachers";
 import Bar from "../../components/Bar";
+import FourCourse from "../../components/FourCourse";
 import { titleList } from "../../service/jsData/title";
 import { intro_horse, purpose_title } from "../../service/jsData/about";
 
@@ -70,6 +71,47 @@ function Guide() {
   );
 }
 
+const p1 =
+  "经过长时间的教学沉淀，如一设计工作室已经建立起一支精通专业，勇于拼搏，富有激情的教师团队，并依靠诚信的为人品格、科学的管理方式，先进的教学理念，严谨的办学作风，如磁铁般吸引更多的优秀老师和各校学生，使我们在设计教学方面拥有扎实的教学素养并富有强大的市场竞争力。舒适优越的学习环境提供了教基础保障，良师益友的教学氛围让学生可以尽情发挥自己的学习热情，一对一的小班制教学也给学生的学习效率上了最重要的保障，我们也保证小白状态来到如一设计，手上功夫满满的离开，让你超越你的同期同学做最优秀的一个。";
+const p2 = "教学富有针对性，对于设计手绘而言。";
+const p3 =
+  "第一是解决手绘基础薄弱问题，百分之九十九的同学手绘基础薄弱，大多都体现在了线条的基本功不扎实，透视运用较不熟练上。第二是解决着色技巧缺乏问题，熟练掌握马克笔做着色是带你水平拔高，画出大神效果图的关键所在。第三是解决有效训练不足问题，灵活运用手绘除了必要的手绘技巧，最重要的是进行大量的针对性练习，熟能生巧才是关键所在。最后是解决学习难以持续的问题，思想懒惰、手上懈怠、都是源于自我目标确缺失、缺乏学习激励及学习任务的科学划分。";
+const p4 =
+  "以上四个问题来到如一，我们都帮你有效解决，把你的学习带入新高度。而且平时不止课上的训练，学校里的专业课作业也可以进行免费辅导，让你在学习上没有阻碍。";
+const p5 =
+  "来到如一设计，不仅帮助你在学习上，多元化的教学环境也可以激发你的爱好兴趣，各路名校大神汇聚一堂，考研和出国的小伙伴也会跟你分享他们的宝贵经验，不同领域的老师也会给你专业化的建议，来到如一，我们就是一家人，我们有最大的空间来尽情释放你的诉求，我们在这里等待您的加入。";
+
+function Welcome() {
+  return (
+    <div className="about-welcome">
+      <h4>
+        <span>A B O U T</span>
+        <span>O U R</span>
+        <span>R U L E S</span>
+      </h4>
+      <p>{p1}</p>
+      <p>{p2}</p>
+      <p>{p3}</p>
+      <p>{p4}</p>
+      <p>{p5}</p>
+      <div className="big">
+        <h2>如一</h2>
+        <h2>等待您的加入</h2>
+      </div>
+    </div>
+  );
+}
+
+function Divider(props) {
+  const { text } = props;
+  return (
+    <div className="about-divider">
+      <p>{text}</p>
+      <div className="line" />
+    </div>
+  );
+}
+
 export default class About extends Component {
   render() {
     return (
@@ -78,7 +120,12 @@ export default class About extends Component {
         <Purpose />
         <Intro />
         <Guide />
+        <Welcome />
+        <Divider />
         <Teachers />
+        <Divider text={"工作室环境"} />
+        <Divider text={"班级设置"} />
+        <FourCourse />
         <Schools />
         <RyMap />
         <Location />
