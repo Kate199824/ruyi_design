1adacf865e1b353e50c3555ae59377db19edfbd1
靳炳淑 <jinbingshu_nju@qq.com>
2019-12-11 22:13:46 +0800

    change router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
-import routeList from './router/routeList';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, HashRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import routeList from "./router/routeList";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Header/>
+        <HashRouter>
+          <Header />
           <ScrollToTop />
-            <Switch>{routeList}</Switch>
-          <Footer/>
-        </BrowserRouter>
+          <Switch>{routeList}</Switch>
+          <Footer />
+        </HashRouter>
       </div>
     );
   }
